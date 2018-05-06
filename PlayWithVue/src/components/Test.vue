7b18b47ce1e4d6f994e329d4abaa173b442848a4
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2018-05-06 09:28:50 -0300

    Estrutura do Vue

---
+++ PlayWithVue/src/components/Test.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="test">
+      <input type="text" v-model="title">
+      <h1>{{title}}</h1>
+      <p>{{user.nome}}</p>
+      <p v-if="mostrarNome">{{user.sobrenome}}</p>
+      <ul>
+          <li v-for="item in items">{{item.title}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: 'Ola Mundo',
+                user: {
+                    nome: 'Lucas',
+                    sobrenome: 'Agusuto'
+                },
+                mostrarNome: true,
+                items: [
+                    {title: "Primeiro Item"},
+                    {title: "Segundo Item"},
+                    {title: "Terceiro Item"},
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
+

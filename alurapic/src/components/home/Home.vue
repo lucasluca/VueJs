6a00f38c99de5473592f10511fd1d472ee4b405c
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2018-05-15 16:07:46 -0300

    alteracoes

--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -6,7 +6,7 @@
        <ul class="lista-fotos">
             <li class="lista-fotos-item" v-for="foto in fotosComFiltro" :key="foto.titulo">
                 <meu-painel :titulo="foto.titulo">
-                    <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
+                    <imagem-responsiva :url="foto.url" :titulo="foto.titulo" v-meu-transform/>
                     <meu-botao :confirmacao="true" estilo="padrao" rotulo="remover" tipo="button" @botaoAtivado="remove(foto)"/>
                 </meu-painel>
             </li>
---
+++ alurapic/src/directives/Transform.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+
+Vue.directive('meu-transform', {
+
+    bind(el, binding, vnode) {
+
+        let current = 0;
+        el.addEventListener('dblclick', function () {
+            current += 90;
+            this.style.transform = `rotate(${current}deg)`;
+        });
+
+    }
+
+});
